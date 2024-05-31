21c9697e4c50a01974ab298ebb7ba765e3b0dff8
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-05-31 19:13:50 +0200

    add new project

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Home() {
+  // console.log('Executing...')
+  return (
+    <main>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Course!</h1>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+      <p><Link href="/about">About Us</Link></p>
+    </main>
+  );
+}
