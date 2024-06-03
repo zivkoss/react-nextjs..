8f2dda5706aa1169912b0c98f403be20dfa836d2
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-06-03 21:37:44 +0200

    add new project

--- app/blog/[slug]/page.js
+++
@@ -1,9 +0,0 @@
-export default function BlogPostPage(props) {
-    return (
-        <main>
-            <h1>Blog Post</h1>
-        </main>
-    );
-}
-
-//
--- components/header.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <>
-           <img src="/logo.ping" alt="A server surreounded by magic sparkles." />
-           <h1>Welcome to this NextJS Course!</h1>
-        </>
-    );
-}
